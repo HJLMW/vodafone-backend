e9630572fba40cd8c67e2023d87bcb8d05711909
Hume <hvme@MacBook-Pro-de-Hume.local>
2024-12-16 12:35:09 +0100

    Git fix

--- src/iot/iot.module.ts
+++ src/iot/iot.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { IoTService } from './iot.service';
 import { IoTResolver } from './iot.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { IoT } from './iot.entity';
 
 @Module({
+	imports: [TypeOrmModule.forFeature([IoT])],
   providers: [IoTService, IoTResolver]
 })
 export class IotModule {}
